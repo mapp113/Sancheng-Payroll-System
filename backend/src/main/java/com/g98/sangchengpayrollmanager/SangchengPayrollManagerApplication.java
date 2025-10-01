1c160f354caa74efb012188b972771a0ae995a1d
mapp113 <nguyendangducquang@gmail.com>
2025-10-01 20:36:57 +0700

    Init project

---
+++ backend/src/main/java/com/g98/sangchengpayrollmanager/SangchengPayrollManagerApplication.java
@@ -0,0 +1,13 @@
+package com.g98.sangchengpayrollmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangchengPayrollManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SangchengPayrollManagerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/g98/sangchengpayrollmanager/SangchengPayrollManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g98.sangchengpayrollmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangchengPayrollManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
