880c77d58716b2a2272f18bacfc5a6297ef8d42d
mapp113 <nguyendangducquang@gmail.com>
2025-11-13 20:21:20 +0700

    add approval screen

--- frontend/src/app/_components/employee/request/types.ts
+++ frontend/src/app/_components/employee/request/types.ts
@@ -1,11 +1,26 @@
-export interface ListLeaveResponseData {
+export interface LeaveResponseData {
+  levaveId: string;
   employeeCode: string;
   fromDate: string;
   toDate: string;
   leaveType: string;
+  status: string;
+}
+
+export interface LeaveDetailResponse {
+  id: string;
+  employeeCode: string;
+  fullName: string;
+  fromDate: string;
+  toDate: string;
+  leaveTypeCode: string;
+  status: string;
+  file?: string;
+  reason: string;
+  note?: string;
   duration: string;
   isPaidLeave: boolean;
-  reason: string;
+  approvalDate?: string;
 }
 
 export interface ListOTResponseData {
