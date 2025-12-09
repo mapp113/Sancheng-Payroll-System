c9c99e85963ff692379ac435e549995b4e51f546
mapp113 <nguyendangducquang@gmail.com>
2025-12-09 15:44:48 +0700

    update meta data

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: { children: React.ReactNode }) {
 
     return (
-        <html lang="en">
+        <html lang="vi">
         <body>
         <Providers>{children}</Providers>
         </body>
